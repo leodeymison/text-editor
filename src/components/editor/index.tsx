ca15f72c7d6abda8970fb3234cd4ae79cd58784e
leodeymison <leodeymisonbasquete@gmail.com>
2025-04-27 09:12:04 -0300

    style

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -2,16 +2,33 @@
 
 import { CSSProperties } from "react";
 
+// ICONS
+import {
+  Italic,
+  Bold,
+  Quote,
+  Heading,
+  Link,
+  Code,
+  Braces,
+  List,
+  ListOrdered,
+  AlignLeft,
+  Underline,
+  Strikethrough,
+} from "lucide-react";
+
 // H1, H2, H3, H4, H5, H6, P, SPAN, SMALL
 // A = Link (Tipo: Externo, interno, posição do texto)
+// Fonte
 // Negrito (100, 200, 300, 400, 500, 600, bold)
 // Italico
-// Fonte
 // Aspas
 // Código (Nome nome do arquivo)
 // Bloco de código
 // Lista pontilhada
 // Lista enumerada
+// Center / Left / Rigth / Justify
 // Riscado
 // Sublinhado
 // Chebox
@@ -33,9 +50,61 @@ export type styleType = {
   header: {
     style: CSSProperties;
     item: {
+      events: {
+        hover?: {
+          backgroundColor?: string;
+        };
+      };
       style: CSSProperties;
       elements: {
-        bold: CSSProperties;
+        bold: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        italic: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        quote: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        h: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        link: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        code: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        blockCode: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        list: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        listOrdered: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        align: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        underline: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
+        strikethrough: {
+          style: CSSProperties;
+          item: CSSProperties;
+        };
       };
     };
   };
@@ -55,34 +124,245 @@ export default function TextEditor() {
         padding: "3px",
       },
       item: {
+        events: {
+          hover: {
+            backgroundColor: "#e8e5e5",
+          },
+        },
         style: {
-          padding: "2px 8px",
+          padding: "5px",
           border: "solid 1px #929292",
           borderRadius: "5px",
           cursor: "pointer",
           display: "flex",
           justifyContent: "center",
           alignItems: "centers",
+          marginRight: "5px",
+          transition: "background-color 0.2s",
         },
         elements: {
           bold: {
-            fontWeight: "bold",
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          italic: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          quote: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          h: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          link: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          code: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          blockCode: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          list: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          listOrdered: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          align: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          underline: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
+          },
+          strikethrough: {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            item: {
+              height: "16px",
+            },
           },
         },
       },
     },
   };
+
+  const element: Array<{
+    name: string;
+    icon: any;
+    code:
+      | "bold"
+      | "italic"
+      | "quote"
+      | "h"
+      | "link"
+      | "code"
+      | "blockCode"
+      | "list"
+      | "listOrdered"
+      | "align"
+      | "underline"
+      | "strikethrough";
+  }> = [
+    {
+      name: "Fonte",
+      icon: Heading,
+      code: "h",
+    },
+    {
+      name: "Negrito",
+      icon: Bold,
+      code: "bold",
+    },
+    {
+      name: "Italico",
+      icon: Italic,
+      code: "italic",
+    },
+    {
+      name: "Aspas",
+      icon: Quote,
+      code: "quote",
+    },
+    {
+      name: "Link",
+      icon: Link,
+      code: "link",
+    },
+    {
+      name: "Código",
+      icon: Code,
+      code: "code",
+    },
+    {
+      name: "Bloco de código",
+      icon: Braces,
+      code: "blockCode",
+    },
+    {
+      name: "Lista pontilhada",
+      icon: List,
+      code: "list",
+    },
+    {
+      name: "Lista ordenada",
+      icon: ListOrdered,
+      code: "listOrdered",
+    },
+    {
+      name: "Alinhamento",
+      icon: AlignLeft,
+      code: "align",
+    },
+    {
+      name: "Sublinhado",
+      icon: Underline,
+      code: "underline",
+    },
+    {
+      name: "Riscado",
+      icon: Strikethrough,
+      code: "strikethrough",
+    },
+  ];
   return (
     <div style={style.editor}>
       <ul style={style.header.style}>
-        <li
-          style={{
-            ...style.header.item.style,
-            ...style.header.item.elements.bold,
-          }}
-        >
-          B
-        </li>
+        {element.map((Item, index) => (
+          <li
+            key={index}
+            style={{
+              ...style.header.item.style,
+              ...style.header.item.elements[Item.code].style,
+              ...(style.header.item.events?.hover?.backgroundColor && {
+                "--hover-bg-color":
+                  style.header.item.events.hover.backgroundColor,
+              }),
+            }}
+            className="hover:bg-[var(--hover-bg-color)]"
+          >
+            <Item.icon style={style.header.item.elements[Item.code].item} />
+          </li>
+        ))}
       </ul>
       <div></div>
       <div></div>
